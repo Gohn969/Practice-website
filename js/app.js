82757d226763b88076ecd96703cf7d09f15f04d1
GohnYay <gohnyayhtun4428@gmail.com>
2022-04-12 11:33:51 +0630

    Developing footer section.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) => {
   item.addEventListener("click", function () {
-    let collapsibleNav = this.parentElement;
-    collapsibleNav.classList.toggle("collapsible--expanded");
+    item.classList.toggle("collapsible--expanded");
   });
 });
